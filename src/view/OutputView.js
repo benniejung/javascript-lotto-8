b18f3b7204f4efdeddead985b8407f0063d07ec0
binniejung <ybe1027@naver.com>
2025-11-02 21:40:47 +0900

    feat(OutputView): 당첨 통계를 출력하는 메서드와 수익률 출력 메서드 추가

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -4,8 +4,23 @@ class OutputView {
   printLottos(lottos) {
     Console.print(`\n${lottos.length}개를 구매했습니다.`);
     lottos.forEach((lotto) => {
-      Console.print(`[${lotto.join(", ")}]`);
+      const numbers = [...lotto].sort((a, b) => a - b);
+      Console.print(`[${numbers.join(", ")}]`);
     });
+  }
+
+  printResult(result) {
+    Console.print(`\n당첨 통계\n---\n`);
+    Console.print(`3개 일치 (5,000원) - ${result.fifthPrizeCount}개`);
+    Console.print(`4개 일치 (50,000원) - ${result.fourthPrizeCount}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${result.thirdPrizeCount}개`);
+    Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${result.secondPrizeCount}개`
+    );
+    Console.print(`6개 일치 (2,000,000,000원) - ${result.firstPrizeCount}개`);
+  }
+  printProfitRate(profitRate) {
+    Console.print(`\n총 수익률은 ${profitRate.toFixed(1)}%입니다.\n`);
   }
 }
 
